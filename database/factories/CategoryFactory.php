f18538a73b55b2ff86bc40735c1e034d37d5fa78
Dot <ngcanindoda9.nd@gmail.com>
2021-01-23 14:28:46 +0200

    factory and seed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -6,7 +6,9 @@ use App\Models\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $word = $faker->text;
     return [
-        //
+        'name' => $word,
+        'slug' => str_slug($word)
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -2,11 +2,18 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Models\Question;
 use App\Models\Reply;
 use Faker\Generator as Faker;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function () {
+            return Question::all()->random();
+        },
+        'user_id' => function () {
+            return \App\User::all()->random();
+        }
     ];
 });
